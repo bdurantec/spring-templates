f8a69fa7f1bfe05440aa1713274d591e9ef2e0f7
Bruno Durante <brunodurantec@gmail.com>
2022-10-12 21:49:01 -0300

    Primeiro commit do projeto

---
+++ src/test/kotlin/br/com/bdurantec/springtemplates/SpringTemplatesApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.bdurantec.springtemplates
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTemplatesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
